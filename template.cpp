af43feb9add3e84f1a1f9deb60c1aa95ad76dea8
kentaro <kentaro.ishii@gmail.com>
2019-02-15 16:56:27 +0900

    make template

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <bitset>
+#include <queue>
+#include <algorithm>
+#include <set>
+#include <cassert>
+#include <random>
+#include <chrono>
+using namespace std;
+
+int main(){
+	return 0;
+}
+
